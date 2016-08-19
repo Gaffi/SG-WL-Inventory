108ef260ce250872a4149255b380ee5e90e142ab
James <Gaffi@outlook.com>
2016-08-19 12:31:10 -0700

    Update SG-WL-Inventory.user.js

--- SG-WL-Inventory.user.js
+++ SG-WL-Inventory.user.js
@@ -5,7 +5,7 @@
 // @description  Scans your whitelist for a particular game to see how many on your list own it. Many props ti Sighery for helping me with the API business and for creating the code I butchered to make this.
 // @author       Gaffi
 // icon          
-// supportURL    
+// @supportURL    https://github.com/Gaffi/SG-WL-Inventory
 // @match        https://www.steamgifts.com/account/manage/whitelist*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
